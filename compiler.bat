619d2d0b9b30b4f9992683728fa028f05298dc1a
jamblox3 <jamiespymasterschool@gmail.com>
2024-11-10 21:28:05 +0000

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -2,15 +2,12 @@
 setlocal
 
 rem Compile the kernel source code to an object file
-"C:\Users\jamie\Dropbox\Jamie\projects\zip programs\mingw32\bin\gcc" -O0 -m32 -ffreestanding -c kernel.c -o kernel.o
+gcc -O0 -m32 -ffreestanding -c kernel.c -o kernel.o
 
 rem Link the object file to create a PE executable for the kernel
-"C:\Users\jamie\Dropbox\Jamie\projects\zip programs\mingw32\bin\ld" -m i386pe -O0 -nostdlib -T link.ld kernel.o -o kernel.exe
+ld -m i386pe -O0 -nostdlib -T link.ld kernel.o -o kernel.exe
 
 rem Convert the PE file to a binary file
-"C:\Users\jamie\Dropbox\Jamie\projects\zip programs\mingw32\bin\objcopy" --strip-all -O binary kernel.exe kernel.bin
-
-rem Copy the binary to the desired location
-copy kernel.bin "c:\users\jamie\dropbox\jamie\projects\isos\my cool iso"
+objcopy --strip-all -O binary kernel.exe kernel.bin
 
 endlocal
